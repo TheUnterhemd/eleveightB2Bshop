815dd550c7a14566590373c21d99a39ae618f06e
TheUnterhemd <kodel.thomas83@gmail.com>
2023-02-22 16:05:28 +0100

    wings completed

--- elveightb2b/src/Pages/Wings.jsx
+++ elveightb2b/src/Pages/Wings.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import './Wings.css'
+import { useCollection } from '../hooks/useCollection'
+import { Link } from 'react-router-dom';
 
 export const Wings = () => {
+
+  const {documents,error} = useCollection("Inventory",["category", "==", "Wing"])
+  
+  
+  const filtered = documents?.reduce((acc,document)=>{
+    const exist = acc.find(item => item.itemName === document.itemName)
+    if(!exist){
+      acc.push({itemName: document.itemName, imgURL: document.imgURL})
+    }
+    return acc
+  },[])
+  
+  
+  
   return (
-    <div>Wings</div>
+    <div className='wings'>
+
+      {filtered?.map((item,index)=>(
+        <Link to={`/wings/${item.itemName}/*`} key={index}>
+        <div className="card">
+          <img src={item.imgURL} alt="" />
+          <h3>{item.itemName}</h3>
+        </div></Link>
+      ))}
+    </div>
   )
 }
